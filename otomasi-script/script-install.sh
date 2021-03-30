8b96b0a14bf4b326b0ef926be37febe2bcc24399
Jasnica Huang <jasnica@alumni.pcr.ac.id>
2021-03-30 09:51:37 +0000

    balik ke semula

--- otomasi-script/script-install.sh
+++ otomasi-script/script-install.sh
@@ -4,7 +4,6 @@ service_a = apache2
 service_b = mysql-server
 
 #Instalasi Apache2
- sudo apt-get install $service_a php php-mysql -y
+
 
 #Instalasi MySQL-Server
- sudo apt-get install $service_b -y
